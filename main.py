da23c71159cbac6c4dce515eaff59e47980f36ab
DalekCraft <thompsone5@wit.edu>
2026-02-15 14:00:32 -0500

    Format main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
-from PySide6.QtWidgets import QApplication,QWidget, QVBoxLayout, QHBoxLayout, QComboBox, QLabel, QPushButton
+
+from PySide6.QtCore import Qt
 from PySide6.QtMultimedia import QCamera, QMediaCaptureSession, QMediaDevices
 from PySide6.QtMultimediaWidgets import QVideoWidget
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QComboBox, QLabel, QPushButton
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("ASL Pipe")
         self.setMinimumSize(600, 600)
-        
+
         # Dropdown Selection
         self.camera_dropdown = QComboBox()
         self.camera_dropdown.setPlaceholderText("Select a camera")
@@ -18,12 +20,12 @@ class MainWindow(QWidget):
         self.camera_dropdown.currentIndexChanged.connect(
             self.on_camera_changed
         )
-        
+
         # Refresh Button
         self.refresh_button = QPushButton()
-        self.refresh_button.setText(("Refresh list"))
+        self.refresh_button.setText("Refresh list")
         self.refresh_button.clicked.connect(self.populate_cameras)
-        
+
         # Camera Output - Placeholder for now until MediaPipe and OpenCV are integrated
         self.camera = None
         self.capture_session = QMediaCaptureSession()
@@ -33,13 +35,13 @@ class MainWindow(QWidget):
 
         # MediaPipe Model Output - also placeholder for now
         self.output_label = QLabel("MediaPipe output will appear here")
-        self.output_label.setAlignment(Qt.AlignCenter)
+        self.output_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.output_label.setStyleSheet("font-size: 14px;")
 
         top = QHBoxLayout()
         top.addWidget(self.camera_dropdown)
         top.addWidget(self.refresh_button)
-        
+
         layout = QVBoxLayout(self)
         layout.addLayout(top)
         layout.addWidget(self.video_widget)
@@ -54,7 +56,7 @@ class MainWindow(QWidget):
         if not self.camera_devices:
             self.camera_dropdown.addItem("No cameras found")
             return
-        
+
         self.camera_devices = QMediaDevices.videoInputs()
         for device in self.camera_devices:
             self.camera_dropdown.addItem(device.description())
@@ -68,7 +70,7 @@ class MainWindow(QWidget):
 
         device = self.camera_devices[index]
         self.camera = QCamera(device)
-        
+
         self.camera_dropdown.setPlaceholderText(self.camera_devices[index].description())
         self.capture_session.setCamera(self.camera)
         self.camera.start()
@@ -82,6 +84,7 @@ class MainWindow(QWidget):
 
         self.start_camera(index)
         self.camera_dropdown.setPlaceholderText(self.camera_devices[index].description())
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
